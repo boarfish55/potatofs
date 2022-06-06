534229877c7a9e7c02d7cc3ca017e698a6fa5b50
Pascal Lalonde <plalonde@overnet.ca>
2022-06-05 20:16:48 -0400

    v0.3.2

--- config.h
+++ config.h
@@ -26,7 +26,7 @@
 
 #define PROGNAME     "potatofs"
 #define MGR_PROGNAME "potatomgr"
-#define VERSION      "0.3.1"
+#define VERSION      "0.3.2"
 
 #define FS_DEFAULT_ENTRY_TIMEOUTS 120
 #define FS_DEFAULT_DATA_PATH      "/var/potatofs"
