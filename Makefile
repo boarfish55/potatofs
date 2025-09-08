6d13aff5ca55c27b8b985154bb11c41c8ba8fa4e
Pascal Lalonde <plalonde@overnet.ca>
2025-09-08 17:05:07 -0400

    Add a convenience tag script, use tags to determine version

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
 DEPDIR := .deps
 CC := gcc
+VERSION := $(shell git describe --tags --exact-match || \
+	   echo $(shell git describe --abbrev=0)-dev)
 CFLAGS := -Wall -g -DFUSE_USE_VERSION=26 \
 	$(shell pkg-config --cflags fuse uuid libbsd-overlay) \
-	-fstack-protector-strong
+	-fstack-protector-strong -DVERSION=\"$(VERSION)\"
 LDFLAGS := $(shell pkg-config --libs fuse uuid 'jansson >= 2.9' \
 	libbsd-overlay libbsd-ctor sqlite3 zlib) \
 	-Wl,-z,relro -Wl,-z,now
---
+++ tag.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+set -e
+
+cmd="$1"
+
+cur=$(git describe --abbrev=0)
+cur=${cur#v}
+
+slab_version=$(egrep "^#define +SLAB_VERSION +[0-9]+" slabs.h | awk '{print $3}')
+slabdb_version=$(egrep "^#define +SLABDB_VERSION +[0-9]+" slabdb.h | awk '{print $3}')
+dirinode_format=$(egrep "^#define +DIRINODE_FORMAT +[0-9]+" dirinodes.h | awk '{print $3}')
+
+# We subtract 5 to avoid a big version jump since starting to use this script
+major=$(($slab_version + $slabdb_version + $dirinode_format - 5))
+minor=$(echo $cur | cut -d. -f 2)
+patch=$(echo $cur | cut -d. -f 3)
+
+if [ "$major" -gt "$(echo $cur | cut -d. -f 1)" ]; then
+	minor=0
+	patch=0
+else
+	case $cmd in
+		major)
+			echo "$(basename $0): no changes that require a major version"
+			exit 1
+			;;
+		minor)
+			minor=$(($minor + 1))
+			patch=0
+			;;
+		patch)
+			patch=$(($patch + 1))
+			;;
+		*)
+			echo "Usage: $(basename $0) <major|minor|patch>"
+			exit 2
+			;;
+	esac
+fi
+
+echo "$cur => $major.$minor.$patch"
+
+read -p "Tag it? (y/N) " RESP
+
+if [ "$RESP" = "y" -o "$RESP" = "Y" ]; then
+	git tag -m "v$major.$minor.$patch" "$major.$minor.$patch"
+fi
+
+exit 0
